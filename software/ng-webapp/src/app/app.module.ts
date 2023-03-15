186e7ceeb9281749e6b59d6671eb9fe70bdd9b4a
fibstack-admin <115759740+fibstack-admin@users.noreply.github.com>
2023-03-14 19:44:45 -0500

    upgrade to Angular 15

--- software/ng-webapp/src/app/app.module.ts
+++ software/ng-webapp/src/app/app.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AlertService } from './alert.service';
+import { AlertComponent } from './alert/alert.component';
 
 import { AppComponent } from './app.component';
 import { AppService } from './app.service';
 import { BoardComponent } from './board/board.component';
-import { FormsModule } from '@angular/forms';
-import { AlertService } from './alert.service';
-import { AlertComponent } from './alert/alert.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
