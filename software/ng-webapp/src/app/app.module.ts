d00d12906548be537abc0c3317f417a8e6b1a2eb
fibstack-admin <115759740+fibstack-admin@users.noreply.github.com>
2024-05-26 06:38:02 -0500

    Migrated to the latest angular

--- software/ng-webapp/src/app/app.module.ts
+++ software/ng-webapp/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
@@ -11,20 +11,13 @@ import { BoardComponent } from './board/board.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    BoardComponent,
-    AlertComponent,
-    FooterComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [AppService, AlertService],
-  bootstrap: [AppComponent, HeaderComponent, FooterComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        BoardComponent,
+        AlertComponent,
+        FooterComponent,
+        HeaderComponent
+    ],
+    bootstrap: [AppComponent, HeaderComponent, FooterComponent], imports: [BrowserModule,
+        FormsModule], providers: [AppService, AlertService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
